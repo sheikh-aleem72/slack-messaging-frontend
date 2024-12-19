c8f5eb2b7440daa21487787f6a5f9c170500d47a
aleem <sheikhaleem363@gmail.com>
2024-12-19 09:57:58 +0530

    feat: Added sidebar items

--- src/components/organism/Workspace/WorkspacePanel.jsx
+++ src/components/organism/Workspace/WorkspacePanel.jsx
@@ -1,6 +1,7 @@
+import { SideBarItem } from "@/components/atoms/SideBarItem/SideBarItem";
 import { WorkspacePanelHeader } from "@/components/molecules/Workspace/WorkspacePanelHeader";
 import { useGetWorkspaceById } from "@/hooks/apis/workspaces/useGetWorkspaceById";
-import { AlertTriangleIcon, Loader } from "lucide-react";
+import { AlertTriangleIcon, Loader, MessageSquareTextIcon } from "lucide-react";
 import { useParams } from "react-router-dom";
 
 export const WorkspacePanel = () => {
@@ -27,6 +28,14 @@ export const WorkspacePanel = () => {
   return (
     <div className="flex flex-col h-full bg-slack-medium">
       <WorkspacePanelHeader workspace={workspace} />
+      <div className="flex flex-col px-2 mt-3">
+        <SideBarItem
+          label="Threads"
+          icon={MessageSquareTextIcon}
+          id="threads"
+          variant="active"
+        />
+      </div>
     </div>
   );
 };
